6eb06cd622f650b01e37e8df5c7b56de7833d160
NikolasAndreskiRodrigues <128433982+NikolasAndreskiRodrigues@users.noreply.github.com>
2025-04-23 19:26:42 -0300

    Revert "Try to Fix"
    This reverts commit b7335aee0962a4cbed79045898e114578d7a7339.

--- bayes.py
+++ bayes.py
@@ -3,6 +3,10 @@ import fitz
 from groq import Groq  
 import os
 
+# Caminho dinâmico da imagem
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOGO_PATH = os.path.join(CURRENT_DIR, "logo.png")
+
 # Configurar chave da Groq
 GROQ_API_KEY = "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"
 client = Groq(api_key=GROQ_API_KEY)
@@ -29,7 +33,7 @@ def chat_with_groq(prompt, context):
 # Interface
 def main():
     st.title("Sistema Inteligente de Viagem")
-    st.image("logo.jpg")
+    st.image(LOGO_PATH, width=200, caption="Sistema Inteligente de Viagem")
 
     with st.sidebar:
         st.header("Upload de arquivos PDF")
